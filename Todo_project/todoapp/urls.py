c5e758464c003db2792adf2dc608aee4336d2076
SoliatOmolara <azeezsoleehah@gmail.com>
2023-05-14 16:45:44 +0100

    Added Todo create form

--- Todo_project/todoapp/urls.py
+++ Todo_project/todoapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, TodoList, TodoDetail
+from .views import HomeView, TodoList, TodoDetail, TodoCreate
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('tasks/', TodoList.as_view(),name='tasks'),
-    path('details/<int:pk>/', TodoDetail.as_view(), name='details')
+    path('details/<int:pk>/', TodoDetail.as_view(), name='details'),
+    path('details/create/', TodoCreate.as_view(), name='create'),
 ]
--- Todo_project/todoapp/views.py
+++ Todo_project/todoapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.contrib import messages
+from django.urls import reverse_lazy
 from .models import Todo
 
 # Create your views here.
@@ -16,4 +19,14 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     model = Todo
     context_object_name = 'task'
+    
+class TodoCreate(CreateView):
+    model = Todo
+    fields = ['title', 'description', 'completed']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "The task was created successfully.")
+        return super(TodoCreate, self).form_valid(form)
     
