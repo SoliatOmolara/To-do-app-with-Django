6ba0859c9c146d2d7656bacc39912056259518db
SoliatOmolara <azeezsoleehah@gmail.com>
2023-05-14 16:13:49 +0100

    Added Todo List Details

--- Todo_project/todoapp/urls.py
+++ Todo_project/todoapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, TodoList
+from .views import HomeView, TodoList, TodoDetail
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('tasks/', TodoList.as_view(),name='tasks'),
+    path('details/<int:pk>/', TodoDetail.as_view(), name='details')
 ]
