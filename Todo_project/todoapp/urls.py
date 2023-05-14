a5eebac1f9cd9b54e02204072080406c0e0c154b
SoliatOmolara <azeezsoleehah@gmail.com>
2023-05-14 17:11:13 +0100

    Added the Todo delete option

--- Todo_project/todoapp/urls.py
+++ Todo_project/todoapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomeView, TodoList, TodoDetail, TodoCreate, TodoUpdate
+from .views import HomeView, TodoList, TodoDetail, TodoCreate, TodoUpdate, TodoDelete
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('tasks/', TodoList.as_view(),name='tasks'),
     path('details/<int:pk>/', TodoDetail.as_view(), name='details'),
     path('details/create/', TodoCreate.as_view(), name='create'),
-    path('details/update/<int:pk>', TodoUpdate.as_view(), name='update')
+    path('details/update/<int:pk>', TodoUpdate.as_view(), name='update'),
+    path('details/delete/<int:pk>', TodoDelete.as_view(), name='delete'),
 ]
