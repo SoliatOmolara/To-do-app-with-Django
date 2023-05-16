2b3f5289eee20dddaf0d39b8ddac1b817ba15df6
SoliatOmolara <azeezsoleehah@gmail.com>
2023-05-16 14:07:23 +0100

    Added User profile

--- Todo_project/users/views.py
+++ Todo_project/users/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import login 
 from django.views.generic.edit import FormView
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.models import User 
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from .forms import RegisterForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.views import View
 
 
 class RegisterView(FormView):
@@ -28,3 +33,46 @@ class MyLoginView(LoginView):
     def form_invalid(self, form):
         messages.error(self.request, "Invalid Username or Password")
         return self.render_to_response(self.get_context_data(form=form))
+    
+
+
+# ...
+
+class MyProfile(LoginRequiredMixin, View):
+    def get(self, request):
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+        
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+        
+        return render(request, 'registration/profile.html', context)
+    
+    def post(self,request):
+        user_form = UserUpdateForm(
+            request.POST, 
+            instance=request.user
+        )
+        profile_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            
+            messages.success(request,'Your profile has been updated successfully')
+            
+            return redirect('profile')
+        else:
+            context = {
+                'user_form': user_form,
+                'profile_form': profile_form
+            }
+            messages.error(request,'Error updating you profile')
+            
+            return render(request, 'users/profile.html', context)
